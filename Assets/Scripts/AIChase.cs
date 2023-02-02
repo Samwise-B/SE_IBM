20c6763a8c86d147ee2ee2feb61021a7a8061b61
Michael Watson <psjn83@durham.ac.uk>
2023-02-02 17:58:27 +0000

    Enemy AI Changes

--- Assets/Scripts/AIChase.cs
+++ Assets/Scripts/AIChase.cs
@@ -1,33 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
 
 public class AIChase : MonoBehaviour
 {
-    public GameObject player;
-    public float speed;
-    private float distance;
-    //[SerializeField] private Animator animator;
-    //[SerializeField] private FieldOfView fieldOfView;
+    [SerializeField] GameObject player;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float agroRange;
+
+    private float distanceToPlayer;
+    Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {   
+        distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer < agroRange && distanceToPlayer > 0.01)
+        {
+            //chasePlayerX();
+            //chasePlayerY(); //Overwrites x movement
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, moveSpeed);
+        }
+        else
+        {
+            stopChasingPlayer();
+        }
+        
+    }
+    /*
+    bool canSeePlayer(float agroDistance)
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
+        bool val = false;
+        float castDist = agroDistance;
+        Vector2 endPos = cast
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, );    
+    }
+    */
+    void chasePlayerX()
+    {
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed + Time.deltaTime);
+        if (transform.position.x < player.transform.position.x)
+        {
+            //Enemy is on the left of player
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (transform.position.x >= player.transform.position.x)
+        {
+            //Enemy is on the right of player
+            rb2d.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+    }
+    void chasePlayerY() 
+    {
+        if (transform.position.y < player.transform.position.y)
+        {
+            rb2d.velocity = new Vector2(0, moveSpeed);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (transform.position.y > player.transform.position.y)
+        {
+            rb2d.velocity = new Vector2(0, -moveSpeed);
+            transform.localScale = new Vector2(1, 1);
 
-        //animator.SetFloat("Speed", Mathf.Abs(speed));
-
-        //fieldOfView.SetOrigin(transform.position);
+        }
+    }
+    void stopChasingPlayer()
+    {
+        rb2d.velocity = new Vector2(0, 0);
     }
 }
