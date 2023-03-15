2051ec434b8d3c71a8bc50d76b3f2372b18dc591
Samwise-B <sam.burcher99@gmail.com>
2023-03-15 18:48:49 +0000

    Added in-level menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using UnityEngine.Time;
 
 public class MainMenu : MonoBehaviour
 {
@@ -12,10 +13,24 @@ public class MainMenu : MonoBehaviour
 
     public void PlayLevelOne() {
         SceneManager.LoadScene("Level1");
+        if (Time.timeScale == 0) {
+            Time.timeScale = 1;
+        }
     }
 
     public void PlayLevelTwo() {
         SceneManager.LoadScene("Level2");
+        if (Time.timeScale == 0) {
+            Time.timeScale = 1;
+        } 
+    }
+
+    public void togglePause() {
+        if (Time.timeScale == 0) {
+            Time.timeScale = 1;
+        } else {
+            Time.timeScale = 0;
+        }
     }
 
     public void QuitGame ()
